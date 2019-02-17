ad21f83b9f55ed862d88d6b77e79cc2917219e71
saritazavala <42972998+saritazavala@users.noreply.github.com>
2019-02-16 23:32:34 -0600

    Final x2 (Comentarios)

--- Hoja4 Estructuras/src/src/Factory1.java
+++ Hoja4 Estructuras/src/src/Factory1.java
@@ -17,26 +17,27 @@ public class Factory1<E> {
     ///////////////////////////////////////////////////////////////////////////////
 
     public Pila CrearPila(String tipo) {
-        Vector1 vector = new Vector1();
-        Pila pila = null;
+        Vector1 vector = new Vector1(); //Se crea un vector nuevo
+        Pila pila = null; //La nueva pila crada es iguakada a null
 
 
         if (tipo.equals("V")) {
 
-            return new Vector1<E>();
+            return new Vector1<E>(); //si el tipo es igual a V se creara un vector
 
         } if (tipo.equals("A")) {
 
-            return new ArrayList1<E>();
+            return new ArrayList1<E>(); /si el tipo es igual a A se creara un arrayList
         }
         else {
-            return null;
+            return null; //De ni ser ninguna se deja nulo
         }
     }
 
     public ListaAbstracta<E> Crear(String tipo){
 
-
+    //Se aplica lo mismo para los tres tipos de listas
+    //Si es igual se aplica, de lo contrario se regresa nulo    
         if (tipo =="Simple") {
             return new Simple<>();
         }
